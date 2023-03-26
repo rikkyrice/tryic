a778ab2476f6cdb873ee18ef5c858dfa37db28a8
riku_hashiki <Riku.Hashiki@ibm.com>
2023-03-26 15:55:02 +0900

    icos

--- api/app.js
+++ api/app.js
@@ -10,6 +10,24 @@ var meiboRouter = require("./routes/meibo");
 
 var app = express();
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+
+app.use(allowCrossDomain)
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
