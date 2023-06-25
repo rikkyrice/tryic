bcc87cbc3455bb47cd8159d881197fe0056699ff
riku_hashiki <Riku.Hashiki@ibm.com>
2023-06-26 01:24:32 +0900

    fix: db情報更新

--- db/settings.js
+++ db/settings.js
@@ -1,5 +1,5 @@
-exports.host = 'b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud';
-exports.port = 32304;
+exports.host = '19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud';
+exports.port = 30699;
 exports.dbname = 'bludb';
-exports.username = 'hky06906';
-exports.password = 'gSQEtVnlRJQsCZHL';
+exports.username = 'gmt17334';
+exports.password = 'OJPRvkj1HpXNunHW';
